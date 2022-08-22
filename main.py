420c45d43f090c13d8341e4c9c8409b39d2ed3eb
12qie34 <92030429+12qie34@users.noreply.github.com>
2022-08-22 17:22:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_weather():
 
 def get_time():
   nowadays = datetime.date.now()
-  return nowadays.days
+  return nowadays
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
